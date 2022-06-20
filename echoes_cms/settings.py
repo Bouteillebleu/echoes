cd4b7b8885bb44adb87cd2f98bf9fafdf71c43e6
bouteillebleu <isobel.hooper@gmail.com>
2022-06-20 21:16:39 +0100

    Allow us to turn off signups with an environment variable

--- echoes_cms/settings.py
+++ echoes_cms/settings.py
@@ -146,6 +146,8 @@ AUTHENTICATION_BACKENDS = (
     "allauth.account.auth_backends.AuthenticationBackend",
 )
 
+ADAPTER = 'echoes_cms.auth.SocialAccountAdapter'
+
 SITE_ID = 1
 
 LOGIN_REDIRECT_URL = 'index'
